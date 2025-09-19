e12c776baea4c4bca600e340db68a0f79f063f04
IT24102121 <it24102121@my.sliit.lk>
2025-09-19 17:15:15 +0530

    file structure done

---
+++ Web_base_JCB_Management_System/src/main/java/com/example/jcb/web_base_jcb_management_system/WebBaseJcbManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.jcb.web_base_jcb_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBaseJcbManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBaseJcbManagementSystemApplication.class, args);
+    }
+
+}
---
+++ Web_base_JCB_Management_System/src/test/java/com/example/jcb/web_base_jcb_management_system/WebBaseJcbManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jcb.web_base_jcb_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBaseJcbManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
